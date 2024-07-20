f5caa037b490dae6030133dfd70cb3b8ee056f8c
SheikhAbdullahSoft <audiobook.urdunovels@gmail.com>
2024-07-20 07:16:28 +0500

    folders name updated

---
+++ 07-Objects/Task.js
@@ -0,0 +1,18 @@
+console.log("---------Activity 01: Objects: Creation & Access---------");
+
+const bookDetail = {
+  title: "To Kill a Mockingbird",
+  author: "Harper Lee",
+  publishedYear: 1960,
+  publishedDate: "1960-07-11",
+  price: 18.99,
+  publisher: "J.B. Lippincott & Co.",
+  ISBN: "978-0-06-112008-4",
+  genre: "Fiction",
+  pages: 281,
+  language: "English",
+  format: "Hardcover",
+};
+
+console.log(bookDetail);
+console.log(`Title: ${bookDetail.title} -- Author: ${bookDetail.author}`);
